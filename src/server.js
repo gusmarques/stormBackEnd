3f305fbb33ef76ab51c003eaccdd6df23e7d5309
gustavo <gust_fjv@hotmail.com>
2021-02-15 01:16:35 -0300

    edit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +9,8 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://painelStorm:painelStorm@painel-shard-00-00.iibjq.mongodb.net:27017,painel-shard-00-01.iibjq.mongodb.net:27017,painel-shard-00-02.iibjq.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-rspqrp-shard-0&authSource=admin&retryWrites=true&w=majority',{
+mongoose.connect(
+     process.env.MONGO_URL,{
      useNewUrlParser: true,
      useUnifiedTopology:true
 })
@@ -15,4 +18,4 @@ mongoose.connect('mongodb://painelStorm:painelStorm@painel-shard-00-00.iibjq.mon
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-// app.listen(3333);
+app.listen(process.env.PORT || 3333);
